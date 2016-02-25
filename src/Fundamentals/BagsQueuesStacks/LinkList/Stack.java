e6a738092bf23f8abf586ac85442112e78098c2f
mingzailao <mingzailao@163.com>
2016-02-25 21:25:10 +0800

    add some

---
+++ src/Fundamentals/BagsQueuesStacks/LinkList/Stack.java
@@ -0,0 +1,74 @@
+package Fundamentals.BagsQueuesStacks.LinkList;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+
+/**
+ * Created by apple on 16/2/25.
+ */
+public class Stack<Item> implements Iterable<Item> {
+    private Node first;
+    private int N;
+    private class Node{
+        Item item;
+        Node next;
+    }
+    public boolean isEmpty(){
+        return first==null;
+    }
+    public int size(){
+        return N;
+    }
+    public void push(Item item){
+        Node oldfirst=first;
+        first=new Node();
+        first.item=item;
+        first.next=oldfirst;
+        N++;
+    }
+    public Item pop(){
+        Item item=first.item;
+        first=first.next;
+        N--;
+        return item;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+    private class ListIterator implements Iterator<Item> {
+
+        private Node current=first;
+        @Override
+        public boolean hasNext() {
+            return current!=null;
+        }
+
+        @Override
+        public Item next() {
+            Item item=current.item;
+            current=current.next;
+            return item;
+        }
+
+        @Override
+        public void remove() {
+
+        }
+    }
+    public static void main(String[] args){
+        Stack<String> s=new Stack<String>();
+        while (!StdIn.isEmpty()){
+            String item=StdIn.readString();
+            if (!item.equals("-")){
+                s.push(item);
+            }
+            else if (!s.isEmpty())
+                StdOut.print(s.pop()+" ");
+        }
+        StdOut.println("("+s.size()+" left on stack)");
+    }
+}
